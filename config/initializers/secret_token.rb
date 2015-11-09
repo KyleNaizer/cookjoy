6399d9207f56e6c920695272a97fd8216319a834
Kyle Naizer <kyle.naizer@gmail.com>
2015-11-09 03:02:22 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cookjoy::Application.config.secret_key_base = '52a8885c06e284f1bfd82c2a0bd20a23aef8ce538e5404242c03999b2fc77e76080a398d7449e75ca96bd07003410cbb27af44c5f58b70f5a7b557b719f3d38b'
